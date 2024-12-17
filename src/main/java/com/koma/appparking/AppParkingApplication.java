03c75010e8cac70d66212042ce4c62d8df575655
Bartłomiej Koma <bartlomiejkoma@MacBook-Air-Bartomiej.local>
2024-12-17 19:16:43 +0100

    inicjalny commit ze stworzonym projektem

---
+++ src/main/java/com/koma/appparking/AppParkingApplication.java
@@ -0,0 +1,13 @@
+package com.koma.appparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppParkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppParkingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koma/appparking/AppParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koma.appparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppParkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
