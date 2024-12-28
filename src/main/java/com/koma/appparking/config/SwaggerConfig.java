3dc01899a5231d09b24a41e022cd4aa33c9cf830
DackaF <daca2002@wp.pl>
2024-12-28 20:08:24 +0100

    Dodatnie dokumentowania endpointów przy pomocy SWAGGER.

---
+++ src/main/java/com/koma/appparking/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.koma.appparking.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Parking Management API")
+                        .version("1.0")
+                        .description("API do zarządzania parkingami, miejscami parkingowymi, pojazdami i biletami parkingowymi."));
+    }
+}
